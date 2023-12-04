ce75a5ff716ce215decb1f29ec7bd3f801b50b6c
Yakdanol <DanilaOlegovich@yandex.ru>
2023-12-04 18:15:58 +0300

    промежуточный коммит

--- commands_sql.py
+++ commands_sql.py
@@ -5,3 +5,43 @@ SELECT *
 FROM "car_catalog"
 """
 
+dvs_car_Select_All = """
+SELECT *
+FROM "dvs_car"
+"""
+
+electric_car_Select_All = """
+SELECT *
+FROM "electric_car"
+"""
+
+hybrid_car_Select_All = """
+SELECT *
+FROM "hybrid_car"
+"""
+
+colour_of_car_Select_All = """
+SELECT *
+FROM "colour_of_car"
+"""
+
+deals_Select_All = """
+SELECT *
+FROM "deals"
+"""
+
+buyers_Select_All = """
+SELECT *
+FROM "buyers"
+"""
+
+all_car_options_Select_All = """
+SELECT *
+FROM "general_car_options"
+"""
+
+options_Select_All = """
+SELECT *
+FROM "options"
+"""
+
