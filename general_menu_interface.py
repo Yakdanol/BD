54bd1548cf7f0bcca006a7c123cccfecdaf2bdd2
Yakdanol <DanilaOlegovich@yandex.ru>
2023-12-05 18:03:53 +0300

    Наладил работу регистрации админа и расположение кнопок

--- general_menu_interface.py
+++ general_menu_interface.py
@@ -45,9 +45,9 @@ class App_general_menu(ctk.CTk):
         self.menu_frame = Frame(self)
         self.init_menu_frame()
 
-        # Фрейм для четвертого состояния (результат show)
-        self.result_state_frame = Frame(self)
-        self.init_result_state_frame()
+        # Фрейм - Вход для администратора
+        self.login_admin_frame = Frame(self)
+        self.init_login_admin_frame()
 
         # Обработка закрытия окна
         self.protocol("WM_DELETE_WINDOW", lambda: self.on_closing())
@@ -58,97 +58,73 @@ class App_general_menu(ctk.CTk):
         self.client_button = ctk.CTkButton(
             self.menu_frame, text="Войти как клиент", command=client_interface
         )
-        self.client_button.grid(row=0, column=0, padx=300, pady=20, sticky="nsew")
-        self.client_button.configure(width=200, height=40, font=("Arial", 30))
+        self.client_button.grid(row=0, column=0, padx=300, pady=100, sticky="nsew")
+        self.client_button.configure(width=200, height=50, font=("Arial", 30))
 
         # Кнопка "Администратор"
         self.admin_button = ctk.CTkButton(
-            self.menu_frame, text="Войти как администратор", command=self.check_admin_credentials
+            self.menu_frame, text="Войти как администратор", command=self.show_login_admin
         )
-        self.admin_button.grid(row=1, column=0, padx=300, pady=20, sticky="nsew")
-        self.admin_button.configure(width=200, height=40, font=("Arial", 30))
-
-        self.username_entry = Entry(self.menu_frame, font=("Arial", 14), width=30)
-        self.password_entry = Entry(self.menu_frame, font=("Arial", 14), show='*', width=30)
-        self.login_button = ctk.CTkButton(
-            self.menu_frame, text="Войти", command=self.check_admin_credentials
-        )
+        self.admin_button.grid(row=1, column=0, padx=300, pady=10, sticky="nsew")
+        self.admin_button.configure(width=200, height=50, font=("Arial", 30))
 
     # инициализация фрейма - Вход для администратора
     def init_login_admin_frame(self):
-        # Кнопка "Входа"
-        self.client_button = ctk.CTkButton(
-            self.menu_frame, text="", command=client_interface
-        )
-        self.client_button.grid(row=0, column=0, padx=300, pady=20, sticky="nsew")
-        self.client_button.configure(width=200, height=40, font=("Arial", 30))
 
+        # # Элемент для ввода логина
+        # self.temp_login = Entry(self.login_admin_frame, font=("Arial", 14), width=30)
+        # # этот объект - поле для ввода, а не параметр, который мы ввели
+        # self.temp_login.grid(row=0, column=1, padx=300, pady=25, sticky="nsew")
 
-    def toggle_admin_login(self):
-        self.username_entry.grid(row=2, column=0, padx=300, pady=10, sticky="nsew")
-        self.password_entry.grid(row=3, column=0, padx=300, pady=10, sticky="nsew")
-        self.login_button.grid(row=4, column=0, padx=300, pady=20, sticky="nsew")
+        # Элемент для ввода логина
+        self.temp_login = Entry(self.login_admin_frame, font=("Arial", 20), width=30)
+        self.temp_login.grid(row=0, column=0, padx=500, pady=25, sticky="nsew")
 
-    def check_admin_credentials(self):
-        username = self.username_entry.get()
-        password = self.password_entry.get()
+        # Элементы для ввода пароля
+        self.temp_password = Entry(self.login_admin_frame, font=("Arial", 20), width=30)
+        self.temp_password.grid(row=1, column=0, padx=500, pady=25, sticky="nsew")
 
-        if username == "admin" and password == "password":  # Замените эти значения на свои
-            admin_interface()
-        else:
-            ctk.show_error("Ошибка", "Введены неверные данные")
-
-    def init_result_state_frame(self):
-        # Создайте объект стиля
-        style = ttk.Style()
-
-        # Настройте стиль "Treeview"
-        style.configure("Treeview", font=("Arial", 16))  # Измените шрифт и размер текста
-        style.configure("Treeview.Heading", font=("Arial", 18, "bold"))  # Измените шрифт и размер заголовков столбцов
-
-        # Настройте высоту строк в стиле "Treeview"
-        style.configure("Treeview", rowheight=40)
-
-        # Treeview для отображения данных в виде таблицы
-        self.tree = ttk.Treeview(self.result_state_frame, style="Treeview")
-        self.tree.grid(row=0, column=0, padx=(10, 10), pady=(25, 0), sticky="nsew")
-
-        # Создаем виджеты Scrollbar
-        self.vertical_scrollbar = Scrollbar(
-            self.result_state_frame, orient="vertical", command=self.tree.yview
-        )
-        self.vertical_scrollbar.grid(row=0, column=1, sticky="ns")
-
-        self.horizontal_scrollbar = Scrollbar(
-            self.result_state_frame, orient="horizontal", command=self.tree.xview
-        )
-        self.horizontal_scrollbar.grid(row=1, column=0, sticky="ew")
-
-        # Привязываем Scrollbar к Treeview
-        self.tree.configure(
-            yscrollcommand=self.vertical_scrollbar.set,
-            xscrollcommand=self.horizontal_scrollbar.set,
-        )
-
-        # Ограничиваем размеры Treeview
-        self.tree.config(height=25, show="headings")
+        # Кнопка "Подтвердить"
+        self.confirm_button_login_admin = ctk.CTkButton(self.login_admin_frame, text="Подтвердить",
+            command=lambda: self.check_admin_credentials())
+        # отправляем в confirm указатель на функцию, НЕ СТРОКУ
+        self.confirm_button_login_admin.grid(row=2, column=0, padx=300, pady=25, sticky="nsew")
+        self.confirm_button_login_admin.configure(width=200, height=50, font=("Arial", 30))
 
         # Кнопка "Назад"
-        self.back_button_result = ctk.CTkButton(
-            self.result_state_frame,
+        self.back_button_login_admin = ctk.CTkButton(
+            self.login_admin_frame,
             text="Назад",
             command=self.show_menu,
             fg_color="grey",
         )
-        self.back_button_result.grid(
-            row=2, column=0, padx=(10, 10), pady=(0, 50), sticky="nsew"
-        )
-        # self.back_button_result.place(connection=10, y=675)  # Задайте нужные вам координаты кнопки
+        self.back_button_login_admin.grid(row=3, column=0, padx=300, pady=25, sticky="nsew")
+        self.back_button_login_admin.configure(width=200, height=50, font=("Arial", 30))
+
+    def check_admin_credentials(self):
+        username = self.temp_login.get()
+        password = self.temp_password.get()
+
+        if username == "1" and password == "1": # postgres, 12345
+            # Очистить поля для ввода логина и пароля
+            self.temp_login.delete(0, 'end')
+            self.temp_password.delete(0, 'end')
+
+            admin_interface()
+
+        else:
+            messagebox.showerror("Ошибка", "Введены неверные данные")
+            # Очистить поля для ввода логина и пароля
+            self.temp_login.delete(0, 'end')
+            self.temp_password.delete(0, 'end')
+
+            app.show_menu()
 
 
     # инициализация функции - скрытия фреймов
     def hide_all_states(self):
         self.menu_frame.grid_forget()
+        self.login_admin_frame.grid_forget()
 
 
     # TODO padx отвечает за сдвиги таблицы с кнопками по горизонтали
@@ -166,23 +142,15 @@ class App_general_menu(ctk.CTk):
             sticky="nsew",
         )
 
-    def check_admin_credentials(self):
-        username = simpledialog.askstring("Логин", "Введите логин:")
-        password = simpledialog.askstring("Пароль", "Введите пароль:", show='*')
-
-        if username == "admin" and password == "password":  # Замените эти значения на свои
-            admin_interface()
-        else:
-            self.show_error_and_return_to_menu()
-
-    def show_error_and_return_to_menu(self):
-        messagebox.showerror("Ошибка", "Введены неверные данные")
-        app.show_menu()
-
-    # закрытие окна и окончание работы с базой
-    # def on_closing(self, connection):
-    #     connection.close()
-    #     self.destroy()
+    def show_login_admin(self):
+        self.hide_all_states()
+        self.login_admin_frame.grid(
+            row=0,
+            column=0,
+            padx=self.winfo_screenwidth() / 2.5,
+            pady=100,
+            sticky="nsew",
+        )
 
     def on_closing(self):
         self.destroy()
